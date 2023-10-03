f3d14b5816b53bd8dcd78d880ae920aa10d658a6
Iamneo <techadmin@iamneo.ai>
2023-10-03 14:51:19 +0000

    Backup Commit

--- springapp/src/controller/MedicineController.java
+++ springapp/src/controller/MedicineController.java
@@ -4,20 +4,28 @@ import home.coder.project.workspace.springapp.src.model.Medicine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 @RestController
 @RequestMapping("/medicines")
 public class MedicineController {
-
-    @Autowired
-    private MedicineService medicineService;
-
+    private List<Medicine> pharmacySystem = new ArrayList<>();
     @PostMapping("/")
     public boolean addMedicine(@RequestBody Medicine medicine) {
-        return medicineService.addMedicine(medicine);
+        return pharmacySystem.add(medicine);
     }
 
     @PutMapping("/{medicineId}")
     public boolean updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
-        return medicineService.updateMedicine(medicineId, updatedMedicine);
+        for (Medicine medicine : pharmacySystem) {
+            if (medicine.getMedicineId() == medicineId) {
+                medicine.setMedicineName(updatedMedicine.getMedicineName());
+                medicine.setPrice(updatedMedicine.getPrice());
+                medicine.setQuantity(updatedMedicine.getQuantity());
+                medicine.setDescription(updatedMedicine.getDescription());
+                return true; 
+            }
+        }
+        return false;
     }
 }
