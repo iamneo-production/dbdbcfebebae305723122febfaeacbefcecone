06ce745e83d6ccc9a6bc5990dccb7c9b6e8833f1
Iamneo <techadmin@iamneo.ai>
2023-10-03 14:46:19 +0000

    Backup Commit

--- springapp/src/service/MedicineService.java
+++ springapp/src/service/MedicineService.java
@@ -8,9 +8,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public interface MedicineService {
-    boolean addMedicine(Medicine medicine);
-    
-    boolean updateMedicine(int medicineId, Medicine updatedMedicine);
+public class MedicineService {
+    private List<Medicine> pharmacySystem = new ArrayList<>();
+
+    @PostMapping("/")
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        // Add the medicine to the pharmacy system (in-memory list)
+        return pharmacySystem.add(medicine);
+    }
+
+    @PutMapping("/{medicineId}")
+    public boolean updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+        for (Medicine medicine : pharmacySystem) {
+            if (medicine.getMedicineId() == medicineId) {
+                // Update the medicine with the new details
+                medicine.setMedicineName(updatedMedicine.getMedicineName());
+                medicine.setPrice(updatedMedicine.getPrice());
+                medicine.setQuantity(updatedMedicine.getQuantity());
+                medicine.setDescription(updatedMedicine.getDescription());
+                return true; // Successfully updated
+            }
+        }
+        return false; // Medicine not found
+    }
 }
 
